a3ccbdf2f48ce8c2d807ea3cf5ce7f50d30cfe29
somepage <mr.d.grigoryan@gmail.com>
2019-01-17 00:08:43 +0300

    Change callbacks for commands, using chat_data instead of user_data

--- app.py
+++ app.py
@@ -24,8 +24,12 @@ def change_gender(bot, update):
     return GENDER
 
 
-def change_voice(bot, update):
-    gender_id = gender.get(update.message.chat_id, default=voice_default)
+def change_voice(bot, update, chat_data):
+    if chat_data.get(update.message.chat_id, 0):
+        gender_id = chat_data[1]
+    else:
+        gender_id = gender_default
+
     voices_buttons = [[InlineKeyboardButton(text=v, callback_data=v) for v in voices[gender_id]]]
     voices_markup = InlineKeyboardMarkup(voices_buttons)
     bot.send_message(chat_id=update.message.chat_id, text="Choose a voice", reply_markup=voices_markup)
@@ -41,27 +45,67 @@ def change_emotion(bot, update):
     return EMOTION
 
 
-def callback_language(bot, update, user_data):
-    user_data['language'] = languages[update.callback_query.data]
+def callback_language(bot, update, chat_data):
+    query = update.callback_query
+
+    if chat_data.get(query.message.chat_id, 0):
+        chat_data[query.message.chat_id][LANG] = query.data
+    else:
+        chat_data[query.message.chat_id] = [
+            query.data,
+            gender_default,
+            voice_default,
+            emotion_default
+        ]
     bot.answer_callback_query(update.callback_query.id, text="Language successfully changed")
 
 
-def callback_gender(bot, update, user_data):
-    user_data['gender'] = update.callback_query.data
+def callback_gender(bot, update, chat_data):
+    query = update.callback_query
+
+    if chat_data.get(query.message.chat_id, 0):
+        chat_data[query.message.chat_id][GENDER] = query.data
+    else:
+        chat_data[query.message.chat_id] = [
+            language_default,
+            query.data,
+            voice_default,
+            emotion_default
+        ]
     bot.answer_callback_query(update.callback_query.id, text="Gender successfully changed")
 
 
-def callback_voice(bot, update, user_data):
-    user_data['voice'] = update.callback_query.data
+def callback_voice(bot, update, chat_data):
+    query = update.callback_query
+
+    if chat_data.get(query.message.chat_id, 0):
+        chat_data[query.message.chat_id][VOICE] = query.data
+    else:
+        chat_data[query.message.chat_id] = [
+            language_default,
+            gender_default,
+            query.data,
+            emotion_default
+        ]
     bot.answer_callback_query(update.callback_query.id, text="Voice successfully changed")
 
 
-def callback_emotion(bot, update, user_data):
-    user_data['emotion'] = update.callback_query.data
+def callback_emotion(bot, update, chat_data):
+    query = update.callback_query
+
+    if chat_data.get(query.message.chat_id, 0):
+        chat_data[query.message.chat_id][EMOTION] = query.data
+    else:
+        chat_data[query.message.chat_id] = [
+            language_default,
+            gender_default,
+            voice_default,
+            query.data
+        ]
     bot.answer_callback_query(update.callback_query.id, text="Emotion successfully changed")
 
 
-def send_speech(bot, update, user_data):
+def send_speech(bot, update, chat_data):
     pass
 
 
@@ -93,14 +137,14 @@ if __name__ == '__main__':
         entry_points=[
             CommandHandler('change_language', change_language),
             CommandHandler('change_gender', change_gender),
-            CommandHandler('change_voice', change_voice),
+            CommandHandler('change_voice', change_voice, pass_chat_data=True),
             CommandHandler('change_emotion', change_emotion)
         ],
         states={
-            LANG: [CallbackQueryHandler(callback_language, pass_user_data=True)],
-            GENDER: [CallbackQueryHandler(callback_gender, pass_user_data=True)],
-            VOICE: [CallbackQueryHandler(callback_voice, pass_user_data=True)],
-            EMOTION: [CallbackQueryHandler(callback_emotion, pass_user_data=True)],
+            LANG: [CallbackQueryHandler(callback_language, pass_chat_data=True)],
+            GENDER: [CallbackQueryHandler(callback_gender, pass_chat_data=True)],
+            VOICE: [CallbackQueryHandler(callback_voice, pass_chat_data=True)],
+            EMOTION: [CallbackQueryHandler(callback_emotion, pass_chat_data=True)],
         },
         fallbacks=[]
     )
